a930e461c1db8f9711e5294d2df5546ac6a55d73
Ejleone <Ejgithinji@gmail.com>
2022-03-10 10:33:15 +0300

    check balance + disburse api

---
+++ v2/src/transaction/mobilemoney/airtel/checkbalance/checkBalance.spec.js
@@ -0,0 +1,13 @@
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const nock = require('nock');
+
+describe('Airtel Check Balance', () => {
+  it('should work with no error ', () => {});
+
+  it('should throw an Error if Airtel API is down ', () => {
+    nock('').post('');
+  });
+
+  it('should return an wrong country error', () => {});
+});
