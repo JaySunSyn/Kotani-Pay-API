1b1cb654a2ddad19b5480e7f910b94163ff7b053
Ejleone <Ejgithinji@gmail.com>
2022-05-05 15:09:51 +0300

    lint

--- v2/.eslintrc.js
+++ v2/.eslintrc.js
@@ -11,5 +11,16 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
+    'no-console': 1,
+    'no-undef': 0,
+    camelcase: 0,
+    'no-await-in-loop': 1,
+    'consistent-return': 0,
+    'no-underscore-dangle': 0,
+    'no-unused-vars': 0,
+    'max-len': 0,
+    'comma-dangle': 0,
   },
 };
+
+
